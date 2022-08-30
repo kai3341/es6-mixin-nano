ce2a55391419778cae4011f736c501d73c3ed64e
Anton Kovalevich <kai3341@gmail.com>
2022-08-30 08:14:35 +0300

    Fix MRO

--- src/mix.js
+++ src/mix.js
@@ -2,7 +2,9 @@ import injectMixin from "./injectMixin";
 
 export default function mix(BaseClass) {
   const Class = class extends BaseClass {};
-  for (const Mixin of arguments.slice(1)) injectMixin(Class, Mixin);
+  for (const Mixin of arguments.slice(1-arguments.length)) {
+    injectMixin(Class, Mixin);
+  }
   return Class;
 }
 
